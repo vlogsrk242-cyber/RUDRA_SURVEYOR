4cafd1eb4fd5d7c259ef8824cd782beffdf2a3d5
vlogsrk242-cyber <vlogsrk242@gmail.com>
2026-09-15 16:38:48 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2518,7 +2518,7 @@ class SettingsPage extends StatelessWidget {
             Colors.white,
       ),
 
-      body: Listview(
+      body: ListView(
         children: [
           ListTile(
             leading:
